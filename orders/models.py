3f926e1e9313a40490ff51a12a38063369b66879
Ildus Islamov <islamov.mail@yandex.com>
2021-06-10 20:44:03 +0500

    Models done.

---
+++ orders/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('accepted', 'Accepted'),
+        ('failed', 'Failed'),
+    ]
+    status = models.CharField(
+        'Status',
+        max_length=12,
+        choices=STATUS_CHOICES,
+        default='new',
+    )
+    created_at = models.DateTimeField('Creation date', auto_now_add=True)
+    external_id = models.CharField('External identifier', max_length=128)
+
+
+class Product(models.Model):
+    name = models.CharField('Product', max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='details',
+        null=False,
+        blank=False,
+    )
+    amount = models.IntegerField('Amount')
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        related_name='details',
+        blank=False,
+        null=True,
+    )
+    price = models.DecimalField('Price', max_digits=12, decimal_places=2)
