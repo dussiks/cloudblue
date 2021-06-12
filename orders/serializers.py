ea5a8c202375f7521b601dc5d5b0c5a9b6a52928
Ildus Islamov <islamov.mail@yandex.com>
2021-06-12 16:47:47 +0500

    Serializers and views refactored. Create and update should be done more.

--- orders/serializers.py
+++ orders/serializers.py
@@ -4,6 +4,7 @@ from .models import Order, OrderDetail, Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=True)
 
     class Meta:
         model = Product
@@ -11,17 +12,32 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderDetailSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
+    id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = OrderDetail
         fields = ('id', 'product', 'amount', 'price',)
+        depth = 1
 
 
 class OrderSerializer(serializers.ModelSerializer):
     details = OrderDetailSerializer(many=True)
-    created_at = serializers.DateTimeField(format='%d-%m-%Y %H:%M:%S')
+    created_at = serializers.DateTimeField(
+        format='%d-%m-%Y %H:%M:%S',
+        read_only=True
+    )
 
     class Meta:
         model = Order
         fields = ('id', 'status', 'created_at', 'external_id', 'details')
+        read_only_fields = ('id', 'status')
+
+    def create(self, validated_data):
+        details = validated_data.pop('details')
+        order = Order.objects.create(**validated_data)
+        for detail in details:
+            OrderDetail.objects.create(**detail, order=order)
+        return order
+
+
+
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,4 @@
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import mixins, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -20,6 +19,11 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['post'], url_path='accept')
     def change_status_to_accepted(self, request, pk=None):
+        """
+        Checks if the status of existing object is 'new' and changes it to
+        'accepted'. Else, returns sufficient message to user and keeps status
+        without changing.
+        """
         order = get_object_or_404(Order, pk=pk)
         if order.status == Status.NEW:
             order.status = Status.ACCEPTED
@@ -33,6 +37,11 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['post'], url_path='fail')
     def change_status_to_failed(self, request, pk=None):
+        """
+        Checks if the status of existing object is 'new' and changes it to
+        'failed'. Else, returns sufficient message to user and keeps status
+        without changing.
+        """
         order = get_object_or_404(Order, pk=pk)
         if order.status == Status.NEW:
             order.status = Status.FAILED
@@ -44,6 +53,17 @@ class OrderViewSet(viewsets.ModelViewSet):
             status=status.HTTP_405_METHOD_NOT_ALLOWED
         )
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
     def destroy(self, request, *args, **kwargs):
         order = get_object_or_404(Order, pk=self.kwargs.get('pk'))
         if order.status == Status.ACCEPTED:
@@ -53,4 +73,3 @@ class OrderViewSet(viewsets.ModelViewSet):
             )
         self.perform_destroy(order)
         return Response(status=status.HTTP_204_NO_CONTENT)
-
